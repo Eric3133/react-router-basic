2127724af14ee581353e8ed10d0d009b53c78bdc
mahongri <mahongri1993@gmail.com>
2024-05-23 01:47:22 -0400

    add link

--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
-  return <div>this is login</div>
+  const navigate = useNavigate();
+  return (
+    <div>
+      this is login
+      <Link to="/article">to article</Link>
+      <button onClick = {() => navigate('/article')}>to article</button>
+    </div>
+  )
 }
 
 export default Login;
