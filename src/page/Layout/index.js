503cad0b1f50a47c0daaff3b331f04404c6a5989
mahongri <mahongri1993@gmail.com>
2024-05-23 02:34:39 -0400

    router embeld

---
+++ src/page/Layout/index.js
@@ -0,0 +1,15 @@
+import {Link, Outlet} from 'react-router-dom';
+const Layout = () => {
+  return (
+    <div>
+      <h1>Layout</h1>
+      <Link to='/board'>board</Link>
+      <Link to='/about'>about</Link>
+
+      {/* // 二级路由出口 */}
+      <Outlet />
+    </div>
+  );
+}
+
+export default Layout;
